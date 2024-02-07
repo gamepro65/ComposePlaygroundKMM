030670e266d82230e9c6920d9c53c1d04db1ae71
Chris Drury <drurc001@gmail.com>
2024-02-06 23:09:51 -0500

    Add resemblence of navigation

--- client/src/commonMain/kotlin/ui/Playground.kt
+++ client/src/commonMain/kotlin/ui/Playground.kt
@@ -1,25 +1,37 @@
-@file:OptIn(ExperimentalTime::class, ExperimentalAnimationApi::class)
+@file:OptIn(ExperimentalAnimationApi::class)
 
 package ui
 
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.LayoutCoordinates
 import androidx.compose.ui.layout.onPlaced
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import kotlin.random.Random
-import kotlin.time.ExperimentalTime
 import kotlin.time.TimeSource
 
 @Composable
@@ -27,58 +39,139 @@ fun MainPlayground(
     modifier: Modifier = Modifier
 ) {
     PerformanceBox(modifier = modifier.fillMaxSize()) {
-        var targetScreen by remember { mutableStateOf<@Composable () -> Unit>({
-            BoxWithConstraints {
-                with (Random.Default) {
-                    with (LocalDensity.current) {
-                        for (i in 0..5)
-                            for (j in 0..5) {
-                                BouncingHello(
-                                    startOffset = Offset(maxWidth.toPx() * nextFloat(), maxHeight.toPx() * nextFloat()),
-                                    startVelocity = Offset(nextFloat() * 4F, nextFloat() * 4F)
-                                )
-                            }
+        var targetScreen by remember { mutableStateOf(Screens.HOME) }
+        Crossfade(
+            modifier = Modifier.fillMaxSize(), targetState = targetScreen
+        ) { content ->
+            Column {
+                Row(modifier = Modifier.background(Color.LightGray).fillMaxWidth()) {
+                    Button(
+                        modifier = Modifier.padding(8.dp),
+                        onClick = { targetScreen = Screens.HOME },
+                        enabled = targetScreen != Screens.HOME
+                    ) {
+                        Image(
+                            painter = rememberVectorPainter(Icons.Default.ArrowBack),
+                            contentDescription = null
+                        )
                     }
                 }
+
+                when (content) {
+                    Screens.HOME -> WelcomeScreen { targetScreen = it }
+                    Screens.BOUNCING_HELLO -> BouncingPlayground()
+                }
             }
-        }) }
-        Crossfade(
-            modifier = Modifier.fillMaxSize(),
-            targetState = targetScreen
-        ) { content -> content() }
+        }
     }
 }
 
 @Composable
-private fun BouncingHello(
+private fun WelcomeScreen(
+    modifier: Modifier = Modifier, goTo: (Screens) -> Unit
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = "Welcome to the Playground"
+        )
+
+        Button(onClick = { goTo(Screens.BOUNCING_HELLO) }) {
+            Text("Bouncing Demo")
+        }
+    }
+}
+
+@Composable
+private fun BouncingPlayground(
+    modifier: Modifier = Modifier
+) {
+    var bouncingHelloCount by remember { mutableStateOf(0) }
+    Box(modifier = modifier.fillMaxSize()) {
+        for (i in 0 until bouncingHelloCount) {
+            RandomBouncingHello()
+        }
+
+        Row(
+            modifier = Modifier.align(Alignment.BottomCenter).padding(bottom = 8.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            Button(
+                onClick = { bouncingHelloCount-- },
+                enabled = bouncingHelloCount != 0
+            ) {
+                Text("-")
+            }
+
+            Text(text = "Total: $bouncingHelloCount")
+
+            Button(onClick = { bouncingHelloCount++ }) {
+                Text("+")
+            }
+        }
+    }
+}
+
+@Composable
+private fun RandomBouncingHello() {
+    BoxWithConstraints {
+        with(Random.Default) {
+            with(LocalDensity.current) {
+                BouncingSlot(
+                    startOffset = Offset(
+                        maxWidth.toPx() * (.05F + nextFloat() * .5F),
+                        maxHeight.toPx() * (.05F + nextFloat() * .5F)
+                    ), startVelocity = Offset(nextFloat() * 4F, nextFloat() * 4F)
+                ) {
+                    Text(text = "Hello Playground!")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun BouncingSlot(
     modifier: Modifier = Modifier,
     startOffset: Offset = Offset.Zero,
     startVelocity: Offset = Offset(3F, 3F),
+    content: @Composable BoxScope.() -> Unit,
 ) {
     BoxWithConstraints(modifier = modifier.fillMaxSize()) {
         var textSize by remember { mutableStateOf<LayoutCoordinates?>(null) }
         var offset by remember { mutableStateOf(startOffset) }
         var velocity by remember { mutableStateOf(startVelocity) }
-        if (offset.x + (textSize?.size?.width ?: 0) > with(LocalDensity.current) { maxWidth.toPx() } || offset.x < 0) {
-            velocity = velocity.copy(x = -velocity.x)
-        }
-        if (offset.y + (textSize?.size?.height ?: 0) > with(LocalDensity.current) { maxHeight.toPx() } || offset.y < 0) {
-            velocity = velocity.copy(y = -velocity.y)
+        textSize?.let { textSize ->
+            with (LocalDensity.current) {
+                if (offset.x + textSize.size.width > maxWidth.toPx() || offset.x < 0) {
+                    velocity = velocity.copy(x = -velocity.x)
+                }
+
+                if (offset.y + textSize.size.height > maxHeight.toPx() || offset.y < 0) {
+                    velocity = velocity.copy(y = -velocity.y)
+                }
+            }
         }
 
-        Text(
-            modifier = Modifier.offset(
-                with(LocalDensity.current) { offset.x.toDp() },
-                with(LocalDensity.current) { offset.y.toDp() }
-            ).onPlaced { textSize = it },
-            text = "Hello Playground!"
+        Box(
+            modifier =
+                Modifier
+                    .onPlaced { textSize = it }
+                    .graphicsLayer {
+                        translationX = offset.x
+                        translationY = offset.y
+                    },
+            content = content
         )
 
         offset = Offset(offset.x + velocity.x, offset.y + velocity.y)
     }
 }
 
-@OptIn(ExperimentalTime::class)
 @Composable
 private fun PerformanceBox(
     modifier: Modifier = Modifier,
@@ -101,4 +194,8 @@ private fun PerformanceBox(
             text = "FPS: ${(1000.0 / frameTimes.average()).toInt()}"
         )
     }
+}
+
+enum class Screens {
+    HOME, BOUNCING_HELLO
 }
